dc335a4374f6374cf3bb7157ebe42a52e4cb0f73
vincentgmd <vinvinvinvin123@hotmail.com>
2015-04-15 17:04:57 -0300

    lineChart3

--- app/src/main/java/com/example/vin/grapix/LineChart.java
+++ app/src/main/java/com/example/vin/grapix/LineChart.java
@@ -8,7 +8,7 @@ public class LineChart extends Chart{
     int count;
     float sum;
     double mark;
-    int space;
+    int pheight;
     int big;
     double width;
 
@@ -30,14 +30,16 @@ public class LineChart extends Chart{
         count=0;
         mark=0;
         width=canvas.getWidth()/valueList.size();
+        pheight= valueList.get(0);
         while(count<valueList.size()){
             paint.setColor(colorList.get(count%colorList.size()));
             //refer=canvas.getWidth()>canvas.getHeight()?canvas.getHeight():canvas.getWidth();
 
-            Rect r =new Rect ((int)mark,(int)(canvas.getHeight()-(((canvas.getHeight())*valueList.get(count))/big)),(int)(mark+width),((canvas.getClipBounds()).bottom));
+           // Rect r =new Rect ((int)mark,(int)(canvas.getHeight()-(((canvas.getHeight())*valueList.get(count))/big)),(int)(mark+width),((canvas.getClipBounds()).bottom));
             //Rect r =new Rect(0,0,100,100);
-            canvas.drawRect(r,paint);
+            canvas.drawLine((float)mark,(float)(canvas.getHeight()-(((canvas.getHeight())*pheight)/big)),(float)(mark+width),(canvas.getHeight()-(((canvas.getHeight())*valueList.get(count))/big)),paint);
             mark+= (width);
+            pheight= valueList.get(count);
 
 
             count++;
