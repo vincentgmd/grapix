66d7422d22273d87a3c7a8b38abcb869c6846333
vincentgmd <vinvinvinvin123@hotmail.com>
2015-04-15 14:54:01 -0300

    do it

--- app/src/main/java/com/example/vin/grapix/blockChart.java
+++ app/src/main/java/com/example/vin/grapix/blockChart.java
@@ -1,8 +1,52 @@
 package com.example.vin.grapix;
 
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.RectF;
+
 /**
  * Created by vin on 4/6/2015.
  */
-public class blockChart {
+public class BlockChart extends Chart{
+    int count;
+    float sum;
+    double mark;
+    int space;
+    int fafa;
+    int big;
+    double width;
+
+
+    BlockChart( ){
+        super();
+    }
+    void draw(Canvas canvas){
+        count=0;
+        sum=0;
+        big= valueList.get(0);
+        while(count<valueList.size()){
+            if(valueList.get(count)>big){
+                big= valueList.get(count);
+            }
+            sum += valueList.get(count);
+            count++;
+        }
+        count=0;
+        mark=0;
+        width=canvas.getWidth()/valueList.size();
+        while(count<valueList.size()){
+            paint.setColor(colorList.get(count%colorList.size()));
+            //refer=canvas.getWidth()>canvas.getHeight()?canvas.getHeight():canvas.getWidth();
+
+            Rect r =new Rect ((int)mark,(int)(canvas.getHeight()-(((canvas.getHeight())*valueList.get(count))/big)),(int)(mark+width),((canvas.getClipBounds()).bottom));
+            //Rect r =new Rect(0,0,100,100);
+            canvas.drawRect(r,paint);
+            mark+= (width);
+
+
+            count++;
+        }
+
+    }
 
 }
